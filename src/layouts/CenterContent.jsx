f0df7623e69d8d6d071da16ba242c1c26794ed16
LIU Zhao <liuzhaomax@163.com>
2022-09-25 22:51:57 +1000

    breadcrumb

--- src/layouts/CenterContent.jsx
+++ src/layouts/CenterContent.jsx
@@ -1,13 +1,13 @@
 import React from "react"
-import {Breadcrumb, Layout} from "antd"
 import "./CenterContent.css"
 import { Outlet } from "react-router-dom"
+import {Breadcrumb, Layout} from "antd"
 
 const { Content } = Layout
 
 function CenterContent() {
     // const navigate = useNavigate()
-
+    //
     // const findNameByPath = (obj, value) => {
     //     let result = []
     //     for (let key in obj) {
@@ -21,7 +21,7 @@ function CenterContent() {
     //     }
     //     return flatten(result)
     // }
-
+    //
     // const getBreadcrumbNameArray = (obj, value) => {
     //     let pathArr = value.split("/").slice(1).map(elem => "/" + elem)
     //     let result = []
@@ -35,27 +35,27 @@ function CenterContent() {
     //     }
     //     return result
     // }
-
+    //
     // const jumpToHome = () => {
     //     navigate(HOME.FULL_PATH)
     // }
-
+    //
     // let breadcrumbNameArray = getBreadcrumbNameArray(WEBSITE_1, props.location.pathname)
 
     return (
         <Layout style={{ padding: "0 24px 24px" }}>
             <Breadcrumb style={{ margin: "16px 0" }}>
-                {
-                    // props.location.pathname !== "/home" ?
-                    //     <React.Fragment>
-                    //         <Breadcrumb.Item className="bc-home" onClick={jumpToHome}>Home</Breadcrumb.Item>
-                    //         <Breadcrumb.Item>{breadcrumbNameArray[0]}</Breadcrumb.Item>
-                    //         <Breadcrumb.Item>{breadcrumbNameArray[1]}</Breadcrumb.Item>
-                    //         <Breadcrumb.Item>{breadcrumbNameArray[2]}</Breadcrumb.Item>
-                    //     </React.Fragment>
-                    //     :
-                    //     <Breadcrumb.Item>Home</Breadcrumb.Item>
-                }
+                {/*{*/}
+                {/*    props.location.pathname !== "/home" ?*/}
+                {/*        <React.Fragment>*/}
+                {/*            <Breadcrumb.Item className="bc-home" onClick={jumpToHome}>Home</Breadcrumb.Item>*/}
+                {/*            <Breadcrumb.Item>{breadcrumbNameArray[0]}</Breadcrumb.Item>*/}
+                {/*            <Breadcrumb.Item>{breadcrumbNameArray[1]}</Breadcrumb.Item>*/}
+                {/*            <Breadcrumb.Item>{breadcrumbNameArray[2]}</Breadcrumb.Item>*/}
+                {/*        </React.Fragment>*/}
+                {/*        :*/}
+                {/*        <Breadcrumb.Item>Home</Breadcrumb.Item>*/}
+                {/*}*/}
             </Breadcrumb>
             <Content className="content">
                 <Outlet />
